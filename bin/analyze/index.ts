88697e72bb5326dec89bbcc8592b36f943c6e8b1
Matthieu Symoens <matthieu.symoens@gmail.com>
2024-12-29 16:26:06 -0500

    Removed closure from analyze script

--- bin/analyze/index.ts
+++ bin/analyze/index.ts
@@ -9,43 +9,41 @@ import { getTypescriptDisabledRules } from './getTypescriptDisabledRules.js'
 import { getTypescriptRules } from './getTypescriptRules.js'
 import { getVueRules } from './getVueRules.js'
 
-(async (): Promise<void> => {
-  const ruleGetters = {
-    base: getBaseRules,
-    import: getImportRules,
-    jest: getJestRules,
-    stylistic: getStylisticRules,
-    typescript: getTypescriptRules,
-    typescriptDisabled: getTypescriptDisabledRules,
-    vue: getVueRules,
-  }
+const ruleGetters = {
+  base: getBaseRules,
+  import: getImportRules,
+  jest: getJestRules,
+  stylistic: getStylisticRules,
+  typescript: getTypescriptRules,
+  typescriptDisabled: getTypescriptDisabledRules,
+  vue: getVueRules,
+}
 
-  for (const [ruleset, getRules] of Object.entries(ruleGetters)) {
-    process.stdout.write(`Analyzing ${ruleset} rules:`)
+for (const [ruleset, getRules] of Object.entries(ruleGetters)) {
+  process.stdout.write(`Analyzing ${ruleset} rules:`)
 
-    const errors: string[] = []
+  const errors: string[] = []
 
-    const rules = await getRules()
+  const rules = await getRules()
 
-    const projectRules = Object.keys(
-      (await import(`rules/${ruleset}.js`)).default,
-    )
+  const projectRules = Object.keys(
+    (await import(`rules/${ruleset}.js`)).default,
+  )
 
-    rules.forEach((rule) => {
-      if (!projectRules.includes(rule)) {
-        errors.push(`\n- missing: ${rule}`)
-      }
-    })
+  rules.forEach((rule) => {
+    if (!projectRules.includes(rule)) {
+      errors.push(`\n- missing: ${rule}`)
+    }
+  })
 
-    projectRules.forEach((projectRule, index) => {
-      if (!rules.includes(projectRule)) {
-        errors.push(`\n- extraneous: ${projectRule}`)
-      }
-      if (index > 0 && projectRule < projectRules[index - 1]) {
-        errors.push(`\n- unordered: ${projectRules[index - 1]}`)
-      }
-    })
+  projectRules.forEach((projectRule, index) => {
+    if (!rules.includes(projectRule)) {
+      errors.push(`\n- extraneous: ${projectRule}`)
+    }
+    if (index > 0 && projectRule < projectRules[index - 1]) {
+      errors.push(`\n- unordered: ${projectRules[index - 1]}`)
+    }
+  })
 
-    process.stdout.write(`${errors.length ? errors.join('') : ' OK'}\n`)
-  }
-})()
+  process.stdout.write(`${errors.length ? errors.join('') : ' OK'}\n`)
+}
